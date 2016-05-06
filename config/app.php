a480df802ae41475567445a47620f6b8e0272ab4
Ibukun Akinlemibola <ibukunakins@gmail.com>
2016-05-06 09:19:08 +0000

    Added Image Handling Package

--- config/app.php
+++ config/app.php
@@ -151,6 +151,7 @@ return [
         Barryvdh\LaravelIdeHelper\IdeHelperServiceProvider::class,
         Laracasts\Flash\FlashServiceProvider::class,
         Cviebrock\EloquentSluggable\SluggableServiceProvider::class,
+        Intervention\Image\ImageServiceProvider::class,
 
         /*
          * Application Service Providers...
@@ -207,7 +208,8 @@ return [
         'View' => Illuminate\Support\Facades\View::class,
         'Form' => Collective\Html\FormFacade::class,
         'Html' => Collective\Html\HtmlFacade::class,
-        'Flash' => 'Laracasts\Flash\Flash::class',
+        'Flash' => Laracasts\Flash\Flash::class,
+        'Image' => Intervention\Image\Facades\Image::class,
         
 
     ],
